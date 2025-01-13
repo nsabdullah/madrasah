556721a18cf8037d66459be1310200f50ce49adc
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-01-13 20:42:51 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Madrasah Academic Center</title>
+  <link rel="shortcut icon" href="assets/img/madrash-01 1.png" type="image/x-icon">
+  <!-- link css-->
+    <link rel="stylesheet" href="assets/css/style_index.css">
+<style>
+
+</style>
+</head>
+<body>
+    <header class="header">
+        <nav class="nav-container">
+            <button class="hamburger">☰</button>
+            <a href="#" class="logo  header_logo"  ><img src="assets/img/madrash-01 1.png" alt="" srcset=""></a>
+            
+            <ul class="nav-menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Courses</a></li>
+                <li><a href="#">Resources</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            
+            <div class="nav-right">
+                <div class="search-box">
+                    <input type="text" placeholder="Search...">
+                </div>
+                <a href="#" class="btn-login">Login</a>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
+
+
